3d62fb5e610995e51eae5abb2db3f2b9a1818392
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-09-18 17:20:17 +0200

    Add Procfile and web task in package.json

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -6,6 +6,7 @@ export const countAllCards = ({cards}) => cards.length;
 
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
+
 // action name creator
 const reducerName = 'searchString';
 const createActionName = name => `app/${reducerName}/${name}`;
@@ -14,7 +15,7 @@ const createActionName = name => `app/${reducerName}/${name}`;
 export const CHANGE = createActionName('CHANGE');
 
 // action creators
-export const createAction_ChangeSearchString = payload => ({ payload, type: CHANGE });
+export const createAction_changeSearchString = payload => ({payload, type: CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
